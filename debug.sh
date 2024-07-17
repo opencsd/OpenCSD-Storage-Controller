c85cad7b6530952cd30dc7d6ecdf00b81695b1a2
juhyunkil <wngus0331@gmail.com>
2024-07-17 13:17:05 +0900

    debug script update

---
+++ debug.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$1" == "i" ] ; then   
+    while [ -z $PODNAME ]
+    do
+        PODNAME=`kubectl get po -o=name -A --field-selector=status.phase=Running | grep csd-identifier`
+        PODNAME="${PODNAME:4}"
+    done
+    kubectl logs $PODNAME -n storage-controller -f 
+elif [ "$1" == "m" ] ; then  
+    while [ -z $PODNAME ]
+    do
+        PODNAME=`kubectl get po -o=name -A --field-selector=status.phase=Running | grep csd-metric-interface`
+        PODNAME="${PODNAME:4}"
+    done
+    kubectl logs $PODNAME -n storage-controller -f 
+elif [ "$1" == "l" ] ; then  
+    while [ -z $PODNAME ]
+    do
+        PODNAME=`kubectl get po -o=name -A --field-selector=status.phase=Running | grep lba2pba-manager`
+        PODNAME="${PODNAME:4}"
+    done
+    kubectl logs $PODNAME -n storage-controller -f 
+elif [ "$1" == "s" ] ; then  
+    while [ -z $PODNAME ]
+    do
+        PODNAME=`kubectl get po -o=name -A --field-selector=status.phase=Running | grep opencsd-storage-manager`
+        PODNAME="${PODNAME:4}"
+    done
+    kubectl logs $PODNAME -n storage-controller -f 
+elif [ "$1" == "a" ] ; then  
+    while [ -z $PODNAME ]
+    do
+        PODNAME=`kubectl get po -o=name -A --field-selector=status.phase=Running | grep storage-api-server`
+        PODNAME="${PODNAME:4}"
+    done
+    kubectl logs $PODNAME -n storage-controller -f 
+else 
+    echo arg error
+fi
